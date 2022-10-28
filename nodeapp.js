760d2f6c8b738e1a587d6bff231907b3e6b8b19e
meetkosiso <kosiso.gaxion@gmail.com>
2022-10-28 21:05:18 +0100

    fix

---
+++ nodeapp.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+let port = process.env.PORT || 3000;
+app.get("/users", function (req, res) {
+  res.json({
+    slackUsername: "zionxl",
+    backend: true,
+    bio: "Tech advocate, Speaker ",
+  });
+});
+app.listen(port, function () {
+  console.log("nodejs running" + port);
+});
