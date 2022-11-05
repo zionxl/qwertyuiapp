50e686c9014c6d1a07d113db1d91a5f1c549d39a
meetkosiso <kosiso.gaxion@gmail.com>
2022-11-05 14:57:09 +0100

    fix

--- nodeapp.js
+++ nodeapp.js
@@ -69,7 +69,7 @@ app.post("/", function (req, res) {
   res.json({
     slackUsername: "zionxl",
     operation_type: operation_type,
-    result: result,
+    result: parseInt(result),
   });
 });
 
